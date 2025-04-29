6f240ca08c5d83c563cce898287e324e3b546509
bscatutsa <bscatutsa@gmail.com>
2025-04-28 20:54:10 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,18 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+/* mobile navbar menu */
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+
+    if (navLinks.classList.contains('active')) {
+        // If already active, we are closing the menu
+        navLinks.classList.remove('active');
+        menuIcon.innerHTML = "☰"; // back to hamburger
+    } else {
+        // If not active, we are opening the menu
+        navLinks.classList.add('active');
+        menuIcon.innerHTML = "✖"; // change to X
+    }
+}
